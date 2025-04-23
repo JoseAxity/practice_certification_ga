e9f4c08c42034842921aa01103e7db0a6216b1e2
Jose Toledano <jose.toledano@axity.com>
2025-04-22 18:57:12 -0600

    Refactor workflow triggers and update Python script output message

--- docker_github/app.py
+++ docker_github/app.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     args = sys.argv[1:]
     if args:
-        print("Argumentos recibidos:", " ".join(args))
+        print("Argumentos recibidos como parametros:", " ".join(args))
     else:
         print("Hola desde el script Python por defecto.")
